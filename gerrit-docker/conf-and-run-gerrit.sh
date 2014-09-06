8706e1e1fc8d147c37bda60f45c92529c3d35674
Christian Posta <christian.posta@gmail.com>
2014-09-06 14:48:48 -0700

    trying to get sed to work on gitlab port

--- gerrit-docker/conf-and-run-gerrit.sh
+++ gerrit-docker/conf-and-run-gerrit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed -i  's/__GITLAB_IP__/'${GITLAB_PORT_80_TCP_ADDR}'/g' $GERRIT_HOME/gerrit/etc/replication.config
+sed -i  's/__GITLAB_IP__/'${GITLAB_PORT_80_TCP_ADDR}'/g' /home/gerrit/gerrit/etc/replication.config
 
 . /home/gerrit/gerrit/bin/gerrit.sh start
