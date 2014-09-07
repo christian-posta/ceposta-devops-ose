b9922601be64a0463a7a657acc6c071d6faeb712
Christian Posta <christian.posta@gmail.com>
2014-09-07 15:09:26 -0700

    added un/pw env variables for gerrit auth to gitlab

--- gerrit-docker/conf-and-run-gerrit.sh
+++ gerrit-docker/conf-and-run-gerrit.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 sed -i  's/__GITLAB_IP__/'${GITLAB_PORT_80_TCP_ADDR}'/g' /home/gerrit/gerrit/etc/replication.config
+sed -i  's/__GITLAB_USER__/'${GITLAB_USER}'/g' /home/gerrit/gerrit/etc/replication.config
+sed -i  's/__GITLAB_PASSWORD__/'${GITLAB_PASSWORD}'/g' /home/gerrit/gerrit/etc/replication.config
+sed -i  's/__GITLAB_PROJ_ROOT__/'${GITLAB_PROJ_ROOT}'/g' /home/gerrit/gerrit/etc/replication.config
 
 service supervisor start
