ffda9d1e8b8cdb41b7c18324bf4d208fdf9c1960
Christian Posta <christian.posta@gmail.com>
2014-09-11 07:42:39 -0700

    restructured scripts a bit

--- scripts/ssh_wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SSH_KEY=${DIR}/jenkins_id_rsa
-
-exec /usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ${SSH_KEY} $1 $2
