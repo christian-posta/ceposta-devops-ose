ff09982cddfd91bc8d78eabff869703670fa1811
Christian Posta <christian.posta@gmail.com>
2014-09-13 07:35:30 -0700

    need to account for a dynamic ssh port, which should be passed in as part of the first param host:port

--- ose-scripts/create_fuse_container.sh
+++ ose-scripts/create_fuse_container.sh
@@ -3,7 +3,7 @@
 
 if [ "$#" -ne 4 ]; then
     echo "Invalid Number of Input Parameters"
-    echo "args: FuseHost AdminPassword OSEBrokerUrl BuildNumber"
+    echo "args: SSHHost:Port AdminPassword OSEBrokerUrl BuildNumber"
 	exit 1
 fi
 
@@ -13,17 +13,18 @@ fi
 #BUILD_NUMBER=1.0.3
 #PROFILE_NAME=my-rest
 
-FUSE_HOST=$1
+FUSE_HOST=$(echo $1 | cut -d ':' -f 1)
+SSH_PORT=$(echo $1 | cut -d ':' -f 2)
 FUSE_ADMIN_PASSWORD=$1
 OPENSHIFT_BROKER=$3
 BUILD_NUMBER=$4
 PROFILE_NAME=my-rest
 CONTAINER_NAME=$(echo restcontainer${BUILD_NUMBER} | tr -d -c "[:alnum:]")
 
-echo "$CONTAINER_NAME"
+echo "host: $FUSE_HOST, port: $SSH_PORT, containerName: $CONTAINER_NAME"
 
 
-alias ssh2fabric="sshpass -p $FUSE_ADMIN_PASSWORD ssh -p 53562 -o ServerAliveCountMax=100 -o ConnectionAttempts=180 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o LogLevel=ERROR admin@$FUSE_HOST"
+alias ssh2fabric="sshpass -p $FUSE_ADMIN_PASSWORD ssh -p $SSH_PORT -o ServerAliveCountMax=100 -o ConnectionAttempts=180 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o PreferredAuthentications=password -o LogLevel=ERROR admin@$FUSE_HOST"
 
 #
 # create a container with our profile
