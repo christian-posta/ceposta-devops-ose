05989b935e6da8e33340f1d61f92b0e0947bd931
Charles Moulliard <ch007m@gmail.com>
2015-05-27 17:18:26 +0200

    Update script to include ssh_config. Maybe that should be the otherway around --> gogs

--- gerrit-docker/src/main/docker/gerrit/conf-and-run-gerrit.sh
+++ gerrit-docker/src/main/docker/gerrit/conf-and-run-gerrit.sh
@@ -9,4 +9,9 @@ sed -i  's/__GITLAB_PROJ_ROOT__/'${GITLAB_PROJ_ROOT}'/g' /home/gerrit/gerrit/etc
 # Configure Gerrit
 sed -i  's/__AUTH_TYPE__/'${AUTH_TYPE}'/g' /home/gerrit/gerrit/etc/gerrit.config
 
+# Add ssh key imported by Kubernetes to access gogs
+RUN echo "Host Gogs" >> /etc/ssh/ssh_config
+RUN echo "Hostname gogs-http-service.default.local" >> /etc/ssh/ssh_config
+RUN echo "IdentityFile /etc/secret-volume/id-rsa" >> /etc/ssh/ssh_config
+
 service supervisor start
